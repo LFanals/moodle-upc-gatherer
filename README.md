f28dc5aee89f16b2b5de70529a81682eb78e1488
LFanals <lfanalsb@gmail.com>
2021-09-10 15:55:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# moodle-up-gatherer
+
+This repository contains a simple script that is able to download all the files present in a Moodle course of Universitat Politècnica de Catalunya (UPC). In case some files are already downloaded in the indicated folder, only new files are downloaded. This program comes from the following needs:
+⋅⋅* To download all the course material once it has finished. I like to keep all the contents of a course for future needs, and it's tedious to download the resources one by one. 
+⋅⋅* To keep a local file system up to date with the Moodle courses. Sometimes the university servers stopped working. In other cases I found myself downloading documents and losing some time on this task. 
+⋅⋅* To keep all the documents/versions that have been uploaded to each of the courses.
+
+ATENEA, the virtual learning environment of Universitat Politècnica de Catalunya (UPC), works with adAS as the authentication manager. This supposes an increase in difficulty to use some softwares that perform similar and more advanced tasks than this program. I couldn't manage to make any of them work.
+
+moodle-upc-gathered is based on Selenium, which makes it very easy to navigate through webpages, and can be programmed to replicate mouse clicks and keyboard strokes. The program exploits the fact that resources uploaded to an ATENEA Moodle course are labelled by an unique identifier, which is currently over 3 million. Thus, it's only necessary to check the identifier to determine if there are new files. Over time, I've came across a few files which have a variable identifier. To avoid saving these files every time the program is executed, the `diff` command is used to determine if the contents of the file already exist within a file of the folder or not.
+
+
+
+## Dependencies
+Google Chrome is required to run this script without problems. In Arch Linux:
+``` 
+$ cd ~/Downloads
+$ git clone https://aur.archlinux.org/google-chrome.git
+$ cd google-chrome/
+$ makepkg -s
+``` 
+
+Make sure you have python installed. In Arch Linux:
+``` 
+$ sudo pacman -S python
+``` 
+
+And then install the required packages:
+``` 
+$ pip install selenium tqdm os glob time
+``` 
+
+
+## Usage
+First, edit `moodle-upc-gatherer.py` with your Moodle username and password:
+```python
+username = ""
+password = ""
+```
+
+Then, type the course links from which you want to gather the documents, the respective directories where files should be saved, and a directory in which files will be downloaded. The program already contains my directories, in case you need an example.
+```python
+courseLinks = []
+dirMain = []
+dirTemporal = ''
+```
+
+Finally, just run the program.
+``` 
+$ python moodle-upc-gatherer.py
+``` 
+
+You can run the program periodically to have your file directories always up to date and not to miss any file.
+
+
+## TODO
+⋅⋅* Gather external resources (contents pointed to by a link). 
+⋅⋅* Improve the folder gathering. 
+⋅⋅* Improve code readability. 
+⋅⋅* Make the program more robust. 
---
+++ moodle-upc-gatherer.py
@@ -0,0 +1,170 @@
+from selenium import webdriver
+from tqdm import tqdm # progress bar
+import os # to rename files
+import glob # to get file name
+import time # to wait for downloads to finish
+
+############### USER config #################
+# Atenea credentials
+username = ""
+password = ""
+# Working directories
+courseLinks = ["https://atenea.upc.edu/course/view.php?id=71639", "https://atenea.upc.edu/course/view.php?id=71638", "https://atenea.upc.edu/course/view.php?id=71729", "https://atenea.upc.edu/course/view.php?id=71685", "https://atenea.upc.edu/course/view.php?id=71645", "https://atenea.upc.edu/course/view.php?id=71715"] # Course links whose resources must be gathered
+dirMain = ["/home/lf/MEE/1_MTP", "/home/lf/MEE/1_MTP", "/home/lf/MEE/2_SCPD", "/home/lf/MEE/3_EDIS", "/home/lf/MEE/4_MOSIC", "/home/lf/MEE/5_IBES"] # Directories in which each course contents are to be stored
+dirTemporal = "/home/lf/MEE/Scripts/Temporal" # Directory where files will be downloaded and temporarily saved
+
+
+# Chrome configuration
+options = webdriver.ChromeOptions()
+options.add_experimental_option('prefs', {
+    "download.default_directory": dirTemporal, # Change default directory for downloads
+    "download.prompt_for_download": False, # To auto download the file
+    "download.directory_upgrade": True,
+    "plugins.always_open_pdf_externally": True # It will not show PDF directly in chrome
+})
+
+# Start Chrome
+driver = webdriver.Chrome(options=options)
+
+print("moodle-upc-gatherer started")
+
+# Login Atenea
+driver.get("https://atenea.upc.edu/login/index.php")
+elem = driver.find_element_by_id("loginbtn")
+elem.click()
+elem = driver.find_element_by_name("adAS_username")
+elem.send_keys(username)
+elem = driver.find_element_by_name("adAS_password")
+elem.send_keys(password)
+elem = driver.find_element_by_name("adAS_submit")
+elem.click()
+
+print("Login is done")
+
+for j in range(len(courseLinks)):
+
+    # Enter course
+    driver.get(courseLinks[j])
+    
+    # Gather all resources links
+    pageSource = driver.page_source
+    
+    # Split pageSource properly
+    Resources = pageSource.split('href="https://atenea.upc.edu/mod/resource/view.php?id=')
+    Resourcess = []
+    for resource in Resources[1:]: # first string [0] is not useful
+        Resourcess.append(resource.split('<span class="accesshide "')[0])
+    
+    
+    # Initialize and fill links, file name and id code vectors
+    vecLinks = []
+    vecCodes = []
+    
+    for resource in Resourcess:
+        code = resource.split('"')[0]
+        vecCodes.append(code)
+        vecLinks.append('https://atenea.upc.edu/mod/resource/view.php?id='+code)
+
+    # # #################### FOLDERS #########################
+    # # Gather the contents inside folders. The files id seems to change, so a lot of diffs are necessary. It can slow down the computer
+    # # Split pageSource properly
+    # Folders = pageSource.split('href="https://atenea.upc.edu/mod/folder/view.php?id=')
+    # Folderss = [] # contains all folder links
+    # for folder in Folders[1:]: 
+    #     Folderss.append('https://atenea.upc.edu/mod/folder/view.php?id=' + folder.split('"')[0])
+
+    # print(Folderss)
+   
+    # for folder in Folderss[0:]:
+    #     driver.get(folder)
+    #     pageSource = driver.page_source
+    #     # Split pageSource properly
+    #     Resources = pageSource.split('href="https://atenea.upc.edu/mod/resource/view.php?id=')
+    #     for resource in Resources[0:]: 
+    #         Resourcess.append(resource.split('<span class="accesshide "')[0])
+    #     # Get resources inside the folder 
+    #     for resource in Resourcess:
+    #         code = resource.split('"')[0]
+    #         vecCodes.append(code)
+    #         vecLinks.append('https://atenea.upc.edu/mod/resource/view.php?id='+code)
+
+
+
+
+
+    # Check id codes
+    
+    vecMainFileNames = glob.glob(dirMain[j]+'/*.*') # get file names in the main folder
+    vecMainCodes = []
+    for i in range(len(vecMainFileNames)):
+        vecMainCodes.append(vecMainFileNames[i].split('/')[-1].split(' ')[0])
+    
+    
+    print("Start gathering at:", courseLinks[j])
+    
+    for i in tqdm(range(len(vecLinks))): # Display progess bar
+        link = vecLinks[i]
+        if (not (vecCodes[i] in vecMainCodes)): # Resource has not been downloaded before
+            try:
+                driver.get(link)
+                # In some uncommon cases, files open/download automatically with the default browser configuration, and because we changed this configuration, they no longer are downloaded.
+                # The following lines gather the root file and download it.
+                pageSource = driver.page_source
+                resourcePlugin = ([word for word in pageSource.split() if word.startswith('href="https://atenea.upc.edu/pluginfile.php/')]) # "https://atenea.upc.edu/pluginfile.php/3833466/mod_resource/content/4/lab3.pdf" 
+                if resourcePlugin: # a pluginfile has been found
+                    linkk = resourcePlugin[0].split('"')[1]
+                    try:
+                        driver.get(linkk)
+                        driver.back() # go back to course main page, so that this same pluginfile is not seen
+    
+                    except:
+                        print("Failed")
+                        continue
+               
+            except:
+                print("Failed")
+                continue
+    
+    print("Waiting until downloads are finished")
+    
+    # Wait until all files are completely downloaded
+    isDownloading = 1
+    while (isDownloading == 1):
+        isDownloading = 0
+        vecFileName = glob.glob(dirTemporal+'/*.*') # get file names
+        for i in range(len(vecFileName)):
+            vecFileName[i] = vecFileName[i].split('/')[-1]
+    
+        for fileName in vecFileName:
+            if fileName.endswith('.crdownload'):
+                isDownloading = 1
+                time.sleep(0.1)
+    
+
+    # Create new files as 'code - name.*' and move them if their content differs (in some unusual cases the code of the same file changes each time, we don't want the same file multiple times in the main folder)
+    vecCurrentMainFiles = glob.glob(dirMain[j]+'/*.*') # get file routes of the files in the main folder
+    for i in range(len(vecFileName)):
+        isNewFile = 1  
+        for k in range(len(vecCurrentMainFiles)):
+            differences = os.system("diff -s " + dirTemporal+'/'+vecFileName[i].replace(' ', '\ ').replace('(', '\(').replace(')', '\)') + " " + vecCurrentMainFiles[k].replace(' ', '\ ').replace('(', '\(').replace(')', '\)') )
+            # differences = os.system("alacritty -e diff -s " + dirTemporal+'/'+vecFileName[i].replace(' ', '\ ').replace('(', '\(').replace(')', '\)') + " " + vecCurrentMainFiles[k].replace(' ', '\ ').replace('(', '\(').replace(')', '\)') ) # To avoid seeing a large log, not an elegant solution though
+            if differences == 0:
+                isNewFile = 0 
+
+        if isNewFile == 1:
+            print(vecFileName[i])
+            os.rename(dirTemporal+'/'+vecFileName[i], dirMain[j]+'/'+vecCodes[i]+' - '+vecFileName[i])
+        else:
+            os.remove(dirTemporal+'/'+vecFileName[i])
+     
+
+driver.close()
+
+print("moodle-upc-gatherer has finished")    
+
+
+
+
+# TODO: seems there's a change the id changes over time. Use diff to check if a new file is really a new file, in terms of content
+
+
